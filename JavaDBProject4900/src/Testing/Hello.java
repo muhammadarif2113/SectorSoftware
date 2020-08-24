959450eefde5dfc6e42edc262b547fdf5b065413
Faisal Ibrahim <faisal.ibrahim@gmail.com>
2020-08-24 01:36:20 -0400

    first internal commit

---
+++ JavaDBProject4900/src/Testing/Hello.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package Testing;
+
+/**
+ * @author faisa
+ *
+ */
+public class Hello {
+
+      /**
+       * @param args
+       */
+      public static void main(String[] args) {
+            // TODO Auto-generated method stub
+            System.out.println("Hi");
+      }
+
+}
