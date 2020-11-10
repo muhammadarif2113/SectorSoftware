16beac2849fb2007460e4775ed1bd938864c71eb
Faisal Ibrahim <faisal.ibrahim@gmail.com>
2020-11-09 21:23:22 -0500

    corrected directory structure

---
+++ src/Testing/Hello.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package Testing;
+
+/**
+ * @author faisa
+ *
+ */
+public class Hello {
+
+      /**
+       * @param args
+       */
+      public static void main(String[] args) {
+            // TODO Auto-generated method stub
+            System.out.println("Hi");
+            System.out.println("Bye");
+      }
+
+}
